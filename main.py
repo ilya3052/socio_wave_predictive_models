afed7040af129177d89147892ac5616670242ab1
ilya3052 <ipopadin5@gmail.com>
2026-05-18 22:20:13 +0300

    вызов функции обучения модели

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from src.collect import get_groups_with_metrics
+from src.predictive_models.train import train_model
+
+
 def main():
-    print("Hello from social-pulse-predictive-models!")
+    groups_ids = [group.id for group in get_groups_with_metrics()]
+    train_result = train_model(groups_ids)
 
 
 if __name__ == "__main__":
