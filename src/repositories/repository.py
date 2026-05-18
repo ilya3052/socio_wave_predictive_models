1b15783ff74bfb1db59efcd28fad3f56100bd4f0
ilya3052 <ipopadin5@gmail.com>
2026-05-18 16:23:57 +0300

    добавление в репозиторий метода получения групп с метриками

--- src/repositories/repository.py
+++ src/repositories/repository.py
@@ -11,8 +11,18 @@ class GroupsRepository(BaseRepository[GroupModel]):
     def __init__(self, session):
         super().__init__(session, GroupModel)
 
-    def get_groups_by_platform(self, platform_id):
-        return self.session.scalars(select(self.model).filter_by(platform_id=platform_id)).all()
+    def get_groups_with_metrics(self):
+        group_ids = self.session.scalars(
+            select(PostMetricsModel.group_id)
+            .filter(PostMetricsModel.timestamp >= datetime.now().date() - timedelta(days=14))
+            .distinct()
+        ).all()
+
+        return self.session.scalars(
+            select(self.model)
+            .filter(self.model.id.in_(group_ids))
+            .options(selectinload(self.model.post_metrics))
+        ).all()
 
     def get_by_external_id(self, external_id):
         return self.session.scalars(select(self.model).filter_by(external_id=external_id)).one_or_none()
